6e8965a0cb632e5b8dd803065276e89a1eb0949c
Felix Pham <felixnapham@hotmail.com>
2020-08-30 22:42:04 -0700

    started on board printer

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,46 @@
 package main;
 
+import board.BigBoard;
+import board.SmallBoard;
+
+import java.util.ArrayList;
+
 public class Main {
+    /*
+    board design:
+    x | o | x   ||   x | o | x   ||   x | o | x
+    -------------------------------------------
+    o | x | o   ||   o | x | o   ||   o | x | o
+    -------------------------------------------
+    x | o | x   ||   x | o | x   ||   x | o | x
+
+    ===========================================
+
+    x | o | x   ||   x | o | x   ||   x | o | x
+    -------------------------------------------
+    o | x | o   ||   o | x | o   ||   o | x | o
+    -------------------------------------------
+    x | o | x   ||   x | o | x   ||   x | o | x
+
+    ===========================================
+
+    x | o | x   ||   x | o | x   ||   x | o | x
+    -------------------------------------------
+    o | x | o   ||   o | x | o   ||   o | x | o
+    -------------------------------------------
+    x | o | x   ||   x | o | x   ||   x | o | x
+     */
+    private static ArrayList listOfBoards(BigBoard board) {
+        ArrayList<SmallBoard> boardList = new ArrayList();
+        for(int i = 1; i <= board.getDimension(); i++) {
+            for(int j = 1; j <= board.getDimension(); j++) {
+                boardList.add(board.getSmallBoard(i, j));
+            }
+        }
+        return boardList;
+    }
+
+    private static String printRow(BigBoard board, int row) {
+        
+    }
 }
